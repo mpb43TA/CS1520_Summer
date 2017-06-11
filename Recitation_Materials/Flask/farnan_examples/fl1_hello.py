5d59b56ab6f8118796ce2c0172fa976adca94467
mbarren <matthewbarren@Matthews-MacBook-Pro-72.local>
2017-06-11 18:13:05 -0400

    added flask files

---
+++ Recitation_Materials/Flask/farnan_examples/fl1_hello.py
@@ -0,0 +1,12 @@
+"""
+Taken from nfarnan github repository cs1520_examples
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
+if __name__ == "__main__":
+	app.run()
